c7f44322736f0cc54e4efbef319f7a5f1d8ea6da
Ansheng Zhou <azhou@lcs.on.ca>
2024-09-27 09:48:08 -0400

    Write out the Test Plans, but some errors occur, double checking it.

---
+++ ISBNTestPlans/ISBNTestPlans.swift
@@ -0,0 +1,16 @@
+//
+//  ISBNTestPlans.swift
+//  ISBNTestPlans
+//
+//  Created by Ansheng Zhou on 2024-09-27.
+//
+
+import Testing
+
+struct ISBNTestPlans {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
