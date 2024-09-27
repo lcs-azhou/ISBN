a66c05c1854388dfdc1b0a6bc9db2e1a207d9f80
Ansheng Zhou <azhou@lcs.on.ca>
2024-09-27 09:23:18 -0400

    The basic functions is completed, missing a String Output of the sum.

--- ISBN/main.swift
+++ ISBN/main.swift
@@ -53,3 +53,53 @@ func getInput(withPrompt prompt: String, minimum: Int?, maximum: Int?) -> Int {
     }
     
 }
+
+func multipleByOne(digit11th: Int, digit13th: Int) -> Int{
+    let result = (digit11th+digit13th)*1
+    return result
+}
+
+func multipleByThree(digit12th: Int) -> Int{
+    let result = (digit12th)*3
+    return result
+}
+
+func sumOfAllMultiples(digit11th:Int, digit12th:Int, digit13th: Int) -> Int{
+    let result = multipleByOne(digit11th: digit11th, digit13th: digit13th) + multipleByThree(digit12th: digit12th)+91
+    return result
+}
+//9780921418=91
+
+func isDividendOfTen(dividend:Int) -> Bool{
+    let dividend = sumOfAllMultiples(digit11th: digit11th, digit12th: digit12th, digit13th: digit13th)
+    if dividend % 10 == 0{
+        return true
+    }else{
+        return false
+    }
+}
+
+func report(on isDividendOfTen: Bool,sumOfAllMultiples: Int) -> String {
+    
+    let result = isDividendOfTen
+    let providedNumber = sumOfAllMultiples
+    
+    if result == true{
+        return "The ISBN number 9780921418\(digit11th)\(digit12th)\(digit13th) is a valid ISBN number.)"
+    } else {
+        return "The ISBN number 9780921418\(digit11th)\(digit12th)\(digit13th) is NOT a valid ISBN number.)"
+    }
+    
+}
+
+let digit11th = getInput(withPrompt: "Digit 11?", minimum: 0, maximum: 9)
+let digit12th = getInput(withPrompt: "Digit 12?", minimum: 0, maximum: 9)
+let digit13th = getInput(withPrompt: "Digit 13?", minimum: 0, maximum: 9)
+
+let output = report(on: isDividendOfTen(dividend: sumOfAllMultiples(digit11th: digit11th, digit12th: digit12th, digit13th: digit13th)), sumOfAllMultiples: sumOfAllMultiples(digit11th: digit11th, digit12th: digit12th, digit13th: digit13th) )
+
+print(output)
+
+
+
+
